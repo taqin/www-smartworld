386983baad3f72f1f2a35b72d27f8d97ac395b63
Tango <hello@taqin.dev>
2025-08-20 16:18:29 +0800

    Remove style

--- src/app/(client-components)/(Header)/MainNav1.tsx
+++ src/app/(client-components)/(Header)/MainNav1.tsx
@@ -29,7 +29,7 @@ const MainNav1: FC<MainNav1Props> = ({ className = "" }) => {
 
         <div className="hidden md:flex flex-shrink-0 justify-end flex-1 lg:flex-none text-neutral-700 dark:text-neutral-100">
           <div className="hidden xl:flex space-x-0.5">
-            <SwitchDarkMode />
+
             <SearchDropdown className="flex items-center" />
             <div className="px-1" />
             <ButtonPrimary className="self-center" href="/login">
